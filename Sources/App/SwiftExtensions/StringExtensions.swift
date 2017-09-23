1fd25ead6a307de214c1eae049666f08717b7a91
johnmorgan <johnpatrickmorganuk@gmail.com>
2017-09-23 20:58:58 +0100

    Rewrites a line that crashes on Linux due to https://bugs.swift.org/browse/SR-4922

--- Sources/App/SwiftExtensions/StringExtensions.swift
+++ Sources/App/SwiftExtensions/StringExtensions.swift
@@ -6,7 +6,10 @@ extension String {
         
         return self
             .components(separatedBy: .newlines)
-            .filter { !CharacterSet(charactersIn: $0).isSubset(of: .whitespacesAndNewlines) }
+            // NOTE: The following line crashes on linux because of https://bugs.swift.org/browse/SR-4922
+            // .filter { !CharacterSet(charactersIn: $0).isSubset(of: .whitespaces) }
+            // Hence, the more convoluted version below:
+            .filter { $0.unicodeScalars.contains(where: { CharacterSet.whitespaces.inverted.contains($0) }) }
             .joined(separator: "\n")
     }
 }
