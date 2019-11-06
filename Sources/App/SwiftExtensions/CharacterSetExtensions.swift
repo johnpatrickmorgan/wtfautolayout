11ae363c96281f2a8ca71bd6d6fdfdeec6425958
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2019-11-06 21:51:07 +0000

    Adds `$` to valid classNameCharacters (fixes #18)

--- Sources/App/SwiftExtensions/CharacterSetExtensions.swift
+++ Sources/App/SwiftExtensions/CharacterSetExtensions.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension CharacterSet {
     
-    static var classNameCharacters: CharacterSet = alphanumerics.union(CharacterSet(charactersIn: "._"))
+    static var classNameCharacters: CharacterSet = alphanumerics.union(CharacterSet(charactersIn: "$._"))
     
     static var urlQueryArgumentAllowed: CharacterSet = urlQueryAllowed.subtracting(CharacterSet(charactersIn: "=&"))
 }
