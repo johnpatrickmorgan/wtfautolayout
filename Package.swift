05d8579b0841d857e6959e8a49c966eca0ed4187
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2019-11-06 21:51:07 +0000

    Updates swift-tools-version and dependencies

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "wtfautolayout",
+    platforms: [
+       .macOS(.v10_13)
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
